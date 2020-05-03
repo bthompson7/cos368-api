94241ad3973ac4bea1e5b01d45d6d1889561ffad
bthompson7 <benjamin.philip.thompson@maine.edu>
2020-05-03 17:28:43 +0000

    fixed it so it works with the app

--- insect_api.py
+++ insect_api.py
@@ -1,4 +1,4 @@
-import os,requests
+import os,requests,keras
 from flask import Flask,render_template,jsonify, request
 from twisted.internet import reactor
 from twisted.web.proxy import ReverseProxyResource
@@ -14,7 +14,7 @@ from io import BytesIO
 from PIL import Image
 
 app = Flask(__name__)
-#app.config["DEBUG"] = True
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -35,15 +35,27 @@ Then it would return:
 {"img_guess":"Hornworm","is_pest":"yes"}
 
 '''
+
+inputTest = re.compile("[^data:image/(jpeg|png|jpg);base64]?[a-zA-Z|0-9|/|,|+]{78,}[=|==]?")
 pests = ["Hornworm","Japanese Beetle","Browntail Moth"]
+
 @app.route('/detect', methods=['POST'])
 def detectInsect():
-	image_data = request.form['img']
+	print("Incoming request...\n")
+	jsonData = request.get_json()
+	image_data = jsonData['img']
+	inputResult = inputTest.match(image_data)
+	if inputResult == None:
+		print("Invalid Input")
+		return jsonify(error="Invalid input"), 404
+	else:
+		print("Input is good")
+
 	image_bytes = str.encode(image_data)
 	type(image_bytes)
 	class_labels = {0: 'Browntail Moth', 1: 'Hornworm',2: 'Japanese Beetle', 3:'Ladybug'}
-	model = load_model('insect_model2.h5') #the model can detect Hornworms and Beetles for now
-	print("reading image...")
+	model = load_model('insect_model2.h5')
+	print("Reading image...\n")
 	image_to_test = 'image_to_test.jpg'
 	with open(image_to_test, "wb") as fh:
 		 fh.write(base64.decodebytes(image_bytes))
@@ -55,12 +67,13 @@ def detectInsect():
     		img = img/255.0
 	pred = model.predict_classes(img,verbose=1)
 	guess = class_labels[pred[0]]
-	print("I think its a %s"%guess)
+	print("I think its a %s\n"%guess)
 
 	is_pest = 'no'
 	for insect in pests:
 		if guess == insect:
 			is_pest = 'yes'
+	keras.backend.clear_session()
 	return jsonify(img_guess=guess,is_pest=is_pest)
 
 resource = WSGIResource(reactor, reactor.getThreadPool(), app)
--- test_insect_api.py
+++ test_insect_api.py
@@ -4,13 +4,14 @@ import base64
 import json
 
 data = {}
-img_to_open = 'hornworm5.jpg'
+img_to_open = 'beetle7.jpg'
 with open(img_to_open, mode='rb') as file:
     img = file.read()
 
-data['img'] = base64.encodebytes(img).decode("utf-8")
-
+data['img'] = base64.encodebytes(img)
+#data['img'] = 'fsfsdfdsfdsfsdfds'
 url = 'http://192.168.1.2:8080/detect'
+print(data)
 r = requests.post(url=url, data=data)
 print(r.status_code, r.reason)
 print(r.text)
